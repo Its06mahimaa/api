c97b40ce3514d190597065ac98b836bb4a88f7ed
Mahima Chauhan <92840571+Its06mahimaa@users.noreply.github.com>
2022-10-17 18:13:56 +0530

    Add files via upload

---
+++ Models/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using Google.Apis.Admin.Directory.directory_v1.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace trainingproject.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        public string Mobile { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Gender { get; set; } = null!;
+
+        [Required]
+        public DateTime Dob { get; set; }
+
+        [Required]
+        public string Hobbies { get; set; } = null!;
+
+        [Required]
+        public IFormFile Profilepic { get; set; } = null;
+
+      
+    }
+}
